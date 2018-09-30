f4922312b2001203cd543470948d2d4ea3efb0f3
Nikolay Savin <info@megawebs.kz>
2018-09-30 12:55:18 +0600

    add en description to snippet's property

--- core/components/utmheaders/lexicon/en/properties.inc.php
+++ core/components/utmheaders/lexicon/en/properties.inc.php
@@ -1,8 +1,3 @@
 <?php
-
-$_lang['utmheaders_prop_limit'] = 'The number of Items to limit per page.';
-$_lang['utmheaders_prop_outputSeparator'] = 'A string to separate each row with.';
-$_lang['utmheaders_prop_sortby'] = 'The field to sort by.';
-$_lang['utmheaders_prop_sortdir'] = 'The direction to sort by.';
-$_lang['utmheaders_prop_tpl'] = 'The chunk to use for each row of Items.';
-$_lang['utmheaders_prop_toPlaceholder'] = 'If set, will output the content to the placeholder specified in this property, rather than outputting the content directly.';
+$_lang['utmheaders_prop_tpl'] = 'the chunk to output';
+$_lang['utmheaders_prop_key'] = 'Tracked GET parameter';
--- core/components/utmheaders/lexicon/ru/properties.inc.php
+++ core/components/utmheaders/lexicon/ru/properties.inc.php
@@ -1,11 +1,3 @@
 <?php
-
-$_lang['utmheaders_prop_limit'] = 'Ограничение вывода вариантов на странице.';
-$_lang['utmheaders_prop_outputSeparator'] = 'Разделитель вывода строк.';
-$_lang['utmheaders_prop_sortby'] = 'Поле сортировки.';
-$_lang['utmheaders_prop_sortdir'] = 'Направление сортировки.';
-$_lang['utmheaders_prop_tpl'] = 'Чанк оформления каждого ряда вариантов.';
-$_lang['utmheaders_prop_toPlaceholder'] = 'Усли указан этот параметр, то результат будет сохранен в плейсхолдер, вместо прямого вывода на странице.';
-
-
+$_lang['utmheaders_prop_tpl'] = 'Чанк вывода';
 $_lang['utmheaders_prop_key'] = 'Отслеживаемый GET параметр';
